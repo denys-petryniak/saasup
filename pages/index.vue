ad5ca78e2eb594e8fd0c66f5eaaeeaa3043de6f5
Denys Petryniak <denys.petryniak@gmail.com>
2023-06-16 21:24:00 +0300

    Use more specific class names for sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts"></script>
 
 <template>
-  <BaseSection class="k-border-debug">
-    <h1 class="section__title">
+  <BaseSection class="hero-section k-border-debug">
+    <h1 class="hero-section__title">
       Build your
       audience and
       grow your brand
     </h1>
-    <p class="section__description">
+    <p class="hero-section__description">
       Make your work easier with an integrated ecosystem that lets all departments work properly together.
     </p>
   </BaseSection>
-  <BaseSection class="k-border-debug">
-    <h2 class="section__title">
+  <BaseSection class="features-section k-border-debug">
+    <h2 class="features-section__title">
       Powerful features to boost
       your productivity
     </h2>
-    <p class="section__description">
+    <p class="features-section__description">
       Use marketing automation to identify hot leads and email your sales team telling them to follow up. Nurture and contact leads at the opportune time to maximize.
     </p>
   </BaseSection>
 </template>
 
 <style scoped lang="scss">
-.section {
+.hero-section {
+  &__title {
+    margin-top: 0;
+  }
+
+  &__description {
+    margin: 0;
+  }
+}
+
+.features-section {
   &__title {
     margin-top: 0;
   }
