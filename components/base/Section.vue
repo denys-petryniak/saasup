bc83579cada19141c3b31e028100e5cb70c63f6e
Denys Petryniak <denys.petryniak@gmail.com>
2023-06-18 21:16:11 +0300

    Use clamp function instead of media queries for section gaps

--- components/base/Section.vue
+++ components/base/Section.vue
@@ -6,18 +6,6 @@
 
 <style scoped lang="scss">
   .section {
-    padding: $gap--small 0;
-  }
-
-  @include breakpoint(small) {
-    .section {
-      padding: $gap--medium 0;
-    }
-  }
-
-  @include breakpoint(medium) {
-    .section {
-      padding: $gap--large 0;
-    }
+    padding: clamp($gap--small, 10vw, $gap--large) 0;
   }
 </style>
