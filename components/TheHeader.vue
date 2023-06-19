fa8275ecb1f41a12431e2a4e55dbf71f31e130ef
Denys Petryniak <denys.petryniak@gmail.com>
2023-06-19 12:18:25 +0300

    Add colors, borders scss variables & create BaseButton component

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,46 @@
 <template>
-  <header>
-    This is a header
+  <header class="header">
+    <NuxtLink class="header__logo-link" to="/">
+      <img class="header__logo" src="@/assets/images/saasup-logo.svg" alt="SaaSup logo">
+    </NuxtLink>
+    <div class="header__main">
+      <BaseButton type="button">
+        Cart
+      </BaseButton>
+      <nav class="navigation">
+        <menu>
+          <NuxtLink to="/">
+            <li>Home</li>
+          </NuxtLink>
+          <NuxtLink to="about">
+            <li>About Us</li>
+          </NuxtLink>
+          <NuxtLink to="">
+            <li>Pages</li>
+          </NuxtLink>
+          <NuxtLink to="contacts">
+            <li>Contact Us</li>
+          </NuxtLink>
+        </menu>
+      </nav>
+    </div>
   </header>
 </template>
 
 <style scoped lang="scss">
+.header {
+  &__logo-link {
+    display: block;
+  }
+
+  &__logo {
+    margin-inline: auto;
+  }
+
+  &__main {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
 </style>
---
+++ components/base/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="btn" v-bind="$attrs">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+.btn {
+  padding: $spacing--large $spacing--xxlarge;
+  background-color: $color--secondary--extra-light;
+  border: 1px solid $color-border--regular;
+  border-radius: $border-radius--xxlarge;
+}
+</style>
