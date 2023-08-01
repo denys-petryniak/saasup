aec9333a993f24963eb23153562536f88fc61f1b
Denys Petryniak <denys.petryniak@gmail.com>
2023-08-01 18:29:46 +0300

    Main page: optimize body background

--- pages/index.vue
+++ pages/index.vue
@@ -314,10 +314,33 @@ const blogSection = ref({
   ],
 })
 
+const img = useImage()
+
+const getBodyBackground = computed(() => {
+  const bodyBackgroundUrl = img('/images/body-background.png', {
+    format: 'webp',
+  })
+  const bodyBackgroundDecorUrl = img('/images/body-background-decor.png', {
+    format: 'webp',
+  })
+
+  return `url('${bodyBackgroundUrl}') no-repeat top right/70%, url('${bodyBackgroundDecorUrl}') no-repeat top right/cover`
+})
+
+// TODO: replace it with something more robust
 useHead({
-  bodyAttrs: {
-    class: 'body-background',
-  },
+  style: [
+    {
+      key: 'body-background',
+      innerHTML: `
+        @media (min-width: 1280px) {
+          body {
+            background: ${getBodyBackground.value};
+          }
+        }
+      `,
+    },
+  ],
 })
 </script>
 
