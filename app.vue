2f81716288dbc4abe678a961313edb11770bbc98
Denys Petryniak <denys.petryniak@gmail.com>
2023-11-14 21:11:02 +0200

    Revert "Disable search engine indexing for preview"
    This reverts commit 2186a629a71222ceded5c2ff2524f768f21a12a6.

--- app.vue
+++ app.vue
@@ -5,15 +5,6 @@ useHead({
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - ${siteTitle}` : siteTitle
   },
-  meta() {
-    const isPreview = useRuntimeConfig().public.NODE_ENV !== 'production'
-
-    return isPreview
-      ? [
-          { hid: 'robots', name: 'robots', content: 'noindex' },
-        ]
-      : []
-  },
 })
 </script>
 
